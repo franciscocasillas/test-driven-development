a8800d9fa0abe6232497e2e2298e3fea0c0ab238
Francisco J. Casillas <chiccasillas@gmail.com>
2023-07-26 14:33:59 -0600

    TDD exercise 2 solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+	let stringNum = "";
+	if (num < 0) {
+		return "ERROR";
+	} else {
+		for (let i = 1; i <= num; i++) {
+			stringNum += string;
+		}
+		return stringNum;
+	}
 };
 
 // Do not edit below this line
