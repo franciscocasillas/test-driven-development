df2529cd321b04e71e39cf37522664d928b1128e
Francisco J. Casillas <chiccasillas@gmail.com>
2023-07-26 15:50:11 -0600

    TDD exercise 4 solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, n1, n2, n3, n4) {
+	let result;
+	result = arr.filter((digit) => digit !== n1);
+	result = result.filter((digit) => digit !== n2);
+	result = result.filter((digit) => digit !== n3);
+	result = result.filter((digit) => digit !== n4);
+	return result;
 };
 
 // Do not edit below this line
