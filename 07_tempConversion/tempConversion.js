534298f0c99dcb3a16c67dde747da83122261471
Francisco J. Casillas <chiccasillas@gmail.com>
2023-07-26 16:36:38 -0600

    TDD exercise 7 solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+	let celcius = (((fahrenheit - 32) * 5) / 9).toFixed(1);
+	return Number(celcius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcius) {
+	let fahrenheit = ((celcius * 9) / 5 + 32).toFixed(1);
+	return Number(fahrenheit);
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
